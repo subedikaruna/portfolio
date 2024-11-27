c8da0c008a95d0aead7ea500344875fb6bb46ef0
Karuna Subedi <karyanna7@gmail.com>
2024-11-27 16:10:43 +0545

    push

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
+// Update the base path to match your repository name
 export default defineConfig({
   plugins: [react()],
- 
+  base: '/portfolio/',
 });
